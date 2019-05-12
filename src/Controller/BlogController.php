de113664b9f66833a1a924032a055c238d1b997e
Maxime Vasseur <zertouflex@gmail.com>
2019-05-12 12:47:36 +0200

    quÃete routing

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class BlogController extends AbstractController
+{
+
+    /**
+     * @Route("/blog/show/{slug<[a-z-0-9-]+>}", name="blog_show")
+     */
+
+
+
+    public function show( $slug)
+    {
+        $slug = ucwords(str_replace("-", " ", $slug));
+        return $this->render('Blog/show.html.twig', [
+            'slug' => $slug,
+        ]);
+    }
+}
