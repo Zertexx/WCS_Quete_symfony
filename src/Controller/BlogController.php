fbe78baf147392ae60eecbf1c300af908b30de30
Maxime Vasseur <zertouflex@gmail.com>
2019-05-13 11:29:44 +0200

    creation entite article et category

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,13 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
 
+
     /**
      * @Route("/blog/show/{slug<[a-z-0-9-]+>}", name="blog_show")
      */
 
 
 
-    public function show( $slug = 'Article Sans Titre')
+    public function show( $slug = 'Article sans titre')
     {
         $slug = ucwords(str_replace("-", " ", $slug));
         return $this->render('Blog/show.html.twig', [
