0c7643aa16716a732f0d0c30e0683af98fc43bac
Maxime Vasseur <zertouflex@gmail.com>
2019-05-30 20:21:51 +0200

    Quete Fixture

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zerto
+ * Date: 30/05/2019
+ * Time: 18:07
+ */
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker;
+
+
+class ArticleFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function getDependencies()
+    {
+        return [CategoryFixtures::class];
+    }
+
+
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 50; $i++)
+        {
+        $faker  =  Faker\Factory::create('fr_FR');
+        $article = new Article();
+            $article->setTitle(mb_strtolower($faker->sentence()));
+            $article->setContent(mb_strtolower($faker->text()));
+
+            $manager->persist($article);
+
+            $article->setCategory($this->getReference('categorie_' . rand(0 , 4)));
+
+            $manager->flush();
+        }
+    }
+
+}
---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use phpDocumentor\Reflection\Types\Self_;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORIES = [
+        'PHP',
+        'Java',
+        'Ruby',
+        'Javascript',
+        'DevOps'
+
+
+
+    ];
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::CATEGORIES as $key => $categoryName)
+        {
+         $category = new Category();
+         $category->setName($categoryName);
+         $manager->persist($category);
+         $this->addReference('categorie_' . $key, $category);
+
+            $manager->flush();
+        }
+    }
+}
